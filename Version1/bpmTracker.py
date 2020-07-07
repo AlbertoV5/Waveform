54c78bcc151a4bdf5c2ee26e9b8ec3365579d442
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2020-07-07 02:24:15 -0500

    Experiments with BPM Tracker

--- Version1/bpmTracker.py
+++ Version1/bpmTracker.py
@@ -7,23 +7,64 @@ Created on Mon Jul  6 17:46:04 2020
 """
 
 import onset
+import os
 
-song = onset.Song("songs/camellia/camellia.mp3")
-
-song.FindAlphaPeak(0, 0.8)
-print(song.peakAlphaIndex)
-
-gen = song.peakAlphaIndex - 1024
-flux, starts = [], []
-for i in range(17):
-    start = gen + i*64
-    end = gen + i*64 + 512
-    chunk = song.data[start:end]
-    rms = onset.GetRMS(chunk)
-    flux.append(rms)
-    starts.append(start)
+def Tempo(fileName):
+    song = onset.Song(fileName)
+    print("RMS: ", song.GetRMS())
     
-realPeak = starts[flux.index(max(flux))]
-print(realPeak)
+    tr = song.CalculateThreshold_RMS()
+    song.FindAlphaPeak(0, tr)
+    
+    print("Peak: ", song.peakAlphaIndex, song.peakAlphaIndex / song.sampfreq)
+    bpms = onset.GetBPMS_All(song, tr)
+    print("Mode BPM:", onset.mode(bpms))
+    print("Median BPM:", onset.median(bpms))
+    print("Mean BPM:", onset.mean(bpms))
 
-bpms = onset.GetBPMS_All(song, 0.8)
+def RMSFlux(fileName):
+    song = onset.Song(fileName)
+    song.FindAlphaPeak(0, 0.8)
+    
+    rms = []
+    quiet = []
+    len_samp = 5 * song.sampfreq
+    for i in range((len(song.data) // len_samp)):
+        start = i * len_samp
+        end = i * len_samp + len_samp
+        power = onset.GetRMS(song.data[start:end])
+        rms.append([start/song.sampfreq, power])
+        print(start, power)
+    '''
+    for i in range(len(rms)-1):
+        a = rms[i + 1][1]
+        b = rms[i][1]
+        if b - a > 10:
+            quiet.append(rms[i + 1])
+
+        
+    before = int(quiet[0][0]*song.sampfreq) - int(5*song.sampfreq)
+    len_samp = int(0.5*song.sampfreq)
+    zoomRMS = []
+    for i in range(11):
+        start = before + len_samp * i 
+        end = before + len_samp * i + len_samp
+        power = onset.GetRMS_100(song.data[start:end])
+        zoomRMS.append([start, power])
+    '''
+    
+#RMSFlux("songs/spectre/spectre.mp3")     
+
+songs = []
+for file in os.listdir(os.getcwd() + "/songs/bpmExperiment"):
+    if "mp3" in file:
+        songs.append(file)
+    
+songs = sorted(songs)
+
+for i in songs:
+    fileName = os.getcwd() + "/songs/bpmExperiment/" + i
+    print(i)
+    Tempo(fileName)
+    print("----------------")
+
--- Version1/onset.py
+++ Version1/onset.py
@@ -33,7 +33,10 @@ class Song():
         self.channels = len(self.data[0])
         if self.channels == 2: # checks for stereo
             self.data = np.add(self.data[:, [0]], self.data[:, [1]]) / self.channels
-            
+        
+        #Normalize to 1 is max
+        self.data = self.data * (1 / np.max(self.data))
+
         if end_sec != 0:
             self.data = self.data[int(start_sec * self.sampfreq):int(end_sec * self.sampfreq)]
             
@@ -45,9 +48,10 @@ class Song():
         rms = 20*np.log10((np.mean(np.absolute(self.data))))
         return int(rms*100)/100
 
-    def FindAlphaPeak(self, start = 0, threshold = 0.8):
+    def FindAlphaPeak(self, start = 0, ratio = 0.8):
+        # MAKE SURE THE DATA IS NORMALIZED SO RATIO = THRESHOLD
         for i in range(len(self.data)):
-            if abs(self.data[i]) > threshold:
+            if abs(self.data[i]) > ratio:
                 self.peakAlpha = self.data[i]
                 self.peakAlphaIndex = i
                 self.length = len(self.data) - self.peakAlphaIndex
@@ -57,8 +61,9 @@ class Song():
     def GetNoteOnset(self, unit = 2048, chunk_size = 2048, threshold_ratio = 0.8, HPF = 20, LPF = 500):
         sus, on, self.notes = -1, -1, []
         note_on = 0
-        threshold = Get_Threshold(self.data, chunk_size, threshold_ratio, HPF, LPF, self.sampfreq)
-        print("Ratio: " + str(threshold_ratio) + " = Threshold: " + str(threshold))
+        song = self.data[self.peakAlphaIndex:]
+        threshold = Get_Threshold(song, chunk_size, threshold_ratio, HPF, LPF, self.sampfreq, self.rms)
+        #print("Ratio: " + str(threshold_ratio) + " = Threshold: " + str(threshold))
                 
         for i in range(self.length//unit):
             start = unit*(i) + self.peakAlphaIndex
@@ -91,7 +96,7 @@ class Song():
             self.pks.append(transientPoint)
             self.pksValue.append(point)
         
-    def GetBPM(self, minBPM = 80, maxBPM = 210, kind = "mode"):
+    def GetBPM(self, minBPM = 100, maxBPM = 210, kind = "mode"):
         x = [i[0] for i in self.notes]
         d = [x[i+1]-x[i] for i in range(len(x)) if i < len(x)-1]
         if kind == "mean":
@@ -102,15 +107,18 @@ class Song():
             beat_s = median(d)/self.sampfreq
         else:
             print("Error")
-        bpm = 60/beat_s
-        while bpm < minBPM or bpm > maxBPM:
-            if bpm < minBPM:
-                bpm = bpm*2
-            elif bpm > maxBPM:
-                bpm = bpm/2
+        if beat_s == 0:
+            bpm = 0
+        else:
+            bpm = 60/beat_s
+            while bpm < minBPM or bpm > maxBPM:
+                if bpm < minBPM:
+                    bpm = bpm*2
+                elif bpm > maxBPM:
+                    bpm = bpm/2
         return bpm
         
-    def GetBPM_PKS(self, minBPM = 80, maxBPM = 210, kind = "mode"):
+    def GetBPM_PKS(self, minBPM = 100, maxBPM = 210, kind = "mode"):
         d = [self.pks[i+1]-self.pks[i] for i in range(len(self.pks)) if i < len(self.pks)-1]
         if kind == "mean":
             beat_s = mean(d)/self.sampfreq
@@ -132,9 +140,25 @@ class Song():
         return bpm 
     
     def CalculateThreshold_RMS(self):
-        rms = GetRMS(self.data)
+        self.rms = GetRMS(self.data)
         floor = -96
-        tr = 1 - (rms/floor)
+        if self.rms > -12:
+            tr = 0.85
+        elif self.rms > -14 and self.rms <= -12:
+            tr = 0.8
+        elif self.rms > -16 and self.rms <= -14:
+            tr = 0.75
+        elif self.rms > -20 and self.rms <= -16:
+            tr = 0.7
+        elif self.rms > -24 and self.rms <= -20:
+            tr = 0.6
+        elif self.rms > -30 and self.rms <= -24.:
+            tr = 0.5
+        else:
+            tr = 0.8
+            
+        #tr = 1 - (self.rms/floor)
+        print("Suggested ratio is: " + str(tr))
         return int(tr * 10000)/10000
 
     def PlotPeaks(self):
@@ -244,33 +268,34 @@ def CalculateFFT_dB(chunk, sampfreq, HPF, LPF):
     return freqsF, FFTF 
 
 
-def Get_Threshold(data, chunk_size, ratio, HPF, LPF, sampfreq):
+def Get_Threshold(data, chunk_size, ratio, HPF, LPF, sampfreq, rms):
         #Find the highest power in the frequency range in the whole data
         #Use it as threshold multiplied by the ratio received
-        high = []
+        total = []
         for i in range(int((len(data)/chunk_size))-1):
             start = chunk_size*(i)
             end = chunk_size*(i) + chunk_size
             chunk = data[start:end]
             
-            FFT = abs(scipy.fft.fft(chunk))
-            freqs = fftpk.fftfreq(len(FFT), (1.0/sampfreq))
-
-            freqs = freqs[range(len(FFT)//2)]
-
-            freqsHPF = freqs[freqs > HPF]
-            freqsLPF = freqs[freqs < LPF]
+            if GetRMS(chunk) > rms*2: # -14 LUFS RMS =  > -28 LUFS, -24 LUFS = > - 48 LUFS
+                FFT = abs(scipy.fft.fft(chunk))
+                freqs = fftpk.fftfreq(len(FFT), (1.0/sampfreq))
+    
+                freqs = freqs[range(len(FFT)//2)]
+    
+                freqsHPF = freqs[freqs > HPF]
+                freqsLPF = freqs[freqs < LPF]
+                
+                indexHPF = int(max(np.where(freqs == freqsHPF[0])))
+                indexLPF = int(max(np.where(freqs == freqsLPF[len(freqsLPF)-1])))
+                
+                FFT = FFT[range(len(FFT)//2)]
+                
+                FFTF = FFT[indexHPF:indexLPF]
+                
+                total.append(np.sum(np.absolute(FFTF)))
             
-            indexHPF = int(max(np.where(freqs == freqsHPF[0])))
-            indexLPF = int(max(np.where(freqs == freqsLPF[len(freqsLPF)-1])))
-            
-            FFT = FFT[range(len(FFT)//2)]
-            
-            FFTF = FFT[indexHPF:indexLPF]
-            
-            high.append(np.sum(np.absolute(FFTF)))
-            
-        threshold = (np.max(high))
+        threshold = (max(total)) - (min(total))
         return threshold * ratio
     
     
@@ -282,7 +307,12 @@ def SavePeaks(peaks, sampfreq, channels, alphaPeak, name):
         file.write(data)
     
 def GetRMS(part):
-    rms = 20*np.log10((np.mean(np.absolute(part))))
+    rms = 20*np.log10((np.mean(np.absolute(part)) + 0.0001))
+    #print("RMS is: " + str(rms) + " dB")
+    return rms
+
+def GetRMS_100(part):
+    rms = (np.mean(np.absolute(part))) * 100
     #print("RMS is: " + str(rms) + " dB")
     return rms
 
@@ -319,7 +349,6 @@ def AmplitudeSum(freqBands):
     for j in freqBands:
         y.append(sum(j))
     return y
-
     
 def GetTopFrequencies(a,b,start,num = 5):
     x, y = list(a), list(b)
@@ -344,10 +373,28 @@ def median(List): #middle of the list
     return sorted(List)[int(len(List)/2)]
 
 def GetBPMS_All(song, tr):
-    return GetBPMS(song, tr), GetBPMS2(song, tr)
+    bpms = []
+    song.GetNoteOnset(unit = 2048, chunk_size = 2048, threshold_ratio = tr, HPF = 0, LPF = 240)
+    song.GetPeaks(x = 1024)
+    magicRatio = (128/129.19921875)
+    bpms.append(int(song.GetBPM()*magicRatio*100)/100)
+    bpms.append(int(song.GetBPM_PKS()*100)/100)
+    song.GetNoteOnset(unit = 2048, chunk_size = 2048, threshold_ratio = tr, HPF = 14000, LPF = 16000)
+    song.GetPeaks(x = 1024)
+    bpms.append(int(song.GetBPM()*magicRatio*100)/100)
+    bpms.append(int(song.GetBPM_PKS()*100)/100)
+    song.GetNoteOnset(unit = 1024, chunk_size = 1024, threshold_ratio = tr, HPF = 0, LPF = 240)
+    song.GetPeaks(x = 512)
+    bpms.append(int(song.GetBPM()*magicRatio*100)/100)
+    bpms.append(int(song.GetBPM_PKS()*100)/100)
+    song.GetNoteOnset(unit = 1024, chunk_size = 1024, threshold_ratio = tr, HPF = 14000, LPF = 16000)
+    song.GetPeaks(x = 512)
+    bpms.append(int(song.GetBPM()*magicRatio*100)/100)
+    bpms.append(int(song.GetBPM_PKS()*100)/100)
+    print(bpms)
+    return bpms
 
 def GetBPMS(song, tr):
-    print("\nCalculating Possible BPMs...")
     bpms1 = BPM_Bass(song, tr)
     bpms2 = BPM_High(song, tr)
     print("\nCalculated possible BPMs:")
@@ -355,7 +402,6 @@ def GetBPMS(song, tr):
     return bpms1, bpms2
 
 def GetBPMS2(song, tr):
-    print("\nCalculating Possible BPMs...")
     bpms1 = BPM_Bass2(song, tr)
     bpms2 = BPM_High2(song, tr)
     print("\nCalculated possible BPMs:")
@@ -364,34 +410,34 @@ def GetBPMS2(song, tr):
 
 def BPM_Bass(song, tr):
     song.GetNoteOnset(unit = 2048, chunk_size = 2048, threshold_ratio = tr, HPF = 0, LPF = 120)
+    song.GetPeaks(x = 1024)
     magicRatio = (128/129.19921875)
-    magicRatio2 = 1/magicRatio
     bpm1 = int(song.GetBPM()*magicRatio*100)/100
-    bpm2 = int(song.GetBPM()*magicRatio2*100)/100
+    bpm2 = int(song.GetBPM_PKS()*100)/100
     return [bpm1, bpm2]
     
 def BPM_High(song, tr):
     song.GetNoteOnset(unit = 2048, chunk_size = 2048, threshold_ratio = tr, HPF = 14000, LPF = 16000)
+    song.GetPeaks(x = 1024)
     magicRatio = (128/129.19921875)
-    magicRatio2 = 1/magicRatio
     bpm1 = int(song.GetBPM()*magicRatio*100)/100
-    bpm2 = int(song.GetBPM()*magicRatio2*100)/100
+    bpm2 = int(song.GetBPM_PKS()*100)/100
     return [bpm1, bpm2]
 
 def BPM_Bass2(song, tr):
     song.GetNoteOnset(unit = 1024, chunk_size = 1024, threshold_ratio = tr, HPF = 0, LPF = 120)
+    song.GetPeaks(x = 512)
     magicRatio = (128/129.19921875)
-    magicRatio2 = 1/magicRatio
     bpm1 = int(song.GetBPM()*magicRatio*100)/100
-    bpm2 = int(song.GetBPM()*magicRatio2*100)/100
+    bpm2 = int(song.GetBPM_PKS()*100)/100
     return [bpm1, bpm2]
     
 def BPM_High2(song, tr):
     song.GetNoteOnset(unit = 1024, chunk_size = 1024, threshold_ratio = tr, HPF = 14000, LPF = 16000)
+    song.GetPeaks(x = 512)
     magicRatio = (128/129.19921875)
-    magicRatio2 = 1/magicRatio
     bpm1 = int(song.GetBPM()*magicRatio*100)/100
-    bpm2 = int(song.GetBPM()*magicRatio2*100)/100
+    bpm2 = int(song.GetBPM_PKS()*100)/100
     return [bpm1, bpm2]
 
 
